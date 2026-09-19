8a08cce817d88b7bb370c625450d25a6ad687d06
dbdnd7rn <144498376+dbdnd7rn@users.noreply.github.com>
2026-09-20 01:44:36 +0200

    Add notifications navigation route

--- src/navigation.ts
+++ src/navigation.ts
@@ -31,6 +31,7 @@ export type RootStack = {
   Emergency: undefined;
   Resources: undefined;
   Profile: undefined;
+  Notifications: undefined;
   StaffWorkspace: undefined;
   StaffManagement: undefined;
   StaffSupportThread: {
